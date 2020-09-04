454b0131a1f1a97b9ecd097d171cb514187d93d4
Jeremy Wood <js.wood206@gmail.com>
2020-09-04 02:37:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require 'pry'
+
+
+class School
+
+  attr_accessor :roster, :name, :grade
+  attr_reader
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.none? { |x| x == grade }
+      binding.pry
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+        @roster[grade] << name
+    end
+  end
+
+end
